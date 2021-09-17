4d6e9412d8e8d371a885767f743abd15bceed9ac
dmilagros <dmilagrosvelasquez@gmail.com>
2021-09-17 00:57:38 -0500

    Access-Control

--- src/services/coursesService.js
+++ src/services/coursesService.js
@@ -3,7 +3,14 @@ import axios from 'axios';
 const baseUrl = 'https://ctc-backend-staging.herokuapp.com/api/';
 
 const getAllCourses = async () => {
-  const res = await axios.get(`${baseUrl}courses/`);
+  const res = await axios.get(`${baseUrl}courses/`,
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+      },
+    });
   return res;
 };
 
